5d6cebe3c70a5f0c0a35cab4243ba354d1ef3c9d
Jimmy Torres <yimmytor@yahoo.es>
2021-06-29 14:47:14 -0600

    Se modifica la ubicacion de App.js a la carpeta components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Cookies from 'universal-cookie';
 import {Fragment} from 'react';
-import HeaderComponent from './components/HeaderComponent/HeaderComponent';
-import MainComponent from './components/MainComponent/MainComponent';
-import FooterComponent from './components/FooterComponent/FooterComponent';
-import ModalComponent from './components/ModalComponent/ModalComponent';
+import HeaderComponent from './HeaderComponent/HeaderComponent';
+import MainComponent from './MainComponent/MainComponent';
+import FooterComponent from './FooterComponent/FooterComponent';
+import ModalComponent from './ModalComponent/ModalComponent';
 
 class App extends React.Component {
   constructor(props) {
@@ -23,8 +23,8 @@ class App extends React.Component {
           message="Utilizamos cookies para mejorar su experiencia en nuestro sitio. Para continuar navegando seleccione la configuración de Cookies que desea utilizar."
           buttons={
             [
-              {method: this.establecerCookies.bind(this, 'todas'), label: 'Aceptar Todas', classButton: 'modalButton'},
-              {method: this.establecerCookies.bind(this, 'necesarias'), label: 'Sólo las necesarias', classButton: 'modalButton'}                            
+              {method: this.establecerCookies.bind(this,'todas'), label: 'Aceptar Todas', classButton: 'modalButton'},
+              {method: this.establecerCookies.bind(this,'necesarias'), label: 'Sólo las necesarias', classButton: 'modalButton'}                            
             ]
           }
         />  
